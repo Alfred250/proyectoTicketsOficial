c76d0001a9aa207fcaa252b861ac771da6e64af8
Alfredo <landerosramirezalfredo@gmail.com>
2025-06-25 09:23:59 -0600

    Cambios en filtros problematicas

--- static/js/TicketsJS.js
+++ static/js/TicketsJS.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
 
-consultarDepartamentos()
+consultarDepartamentos();
 })
 
 async function consultarDepartamentos() {
-  fetch('/filtroDepartamento')
-    .then(response => response.json())
-    .then(data => {
-      console.log("departamento",data)
-      
+  
+  selectDepartamentos=document.getElementById("selectDepartamento");
+  fetch('/filtroDepartamento').then(response => response.json()).then(data => {
+      var departamentosValues= Object.values(data);
+      departamentosValues.forEach(depa => {
+        var opcionDepas= document.createElement("option");
+        opcionDepas.value=Object.keys(data);
+        opcionDepas.text=depa;
+        selectDepartamentos.add(opcionDepas);
       });
+    });
 }
 
-async function consultarProblematicas(params) {
-  fetch('/filtroProblematicas').then(response=> response.json()).then(data=>{
-    console.log("problematicas",data)
-  })
+$("#selectDepartamento").on("change",function(){
+  var depaSeleccionado=this.value;
+  consultarProblematicas(depaSeleccionado)
+})
+
+async function consultarProblematicas(departamento) {
+  var resultado=await fetch("/filtroProblematicas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ departamento })
+      });
+
+      if (resultado.ok){
+        console.log(resultado)
+      }
+
 }
+
