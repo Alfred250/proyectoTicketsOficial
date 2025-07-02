e4a52804add2a5492d2e923439c3342892902051
Alfredo Landeros <66911leo@universidaddeleon.edu.mx>
2025-07-02 11:18:52 -0600

    Obtiene tickets por administrar

--- routers/ticketsRouter.py
+++ routers/ticketsRouter.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,HTTPException
 from fastapi.responses import FileResponse,JSONResponse
-from data.conexionTablaTickets import  ConexionTablaTickets
+from data.ConexionTablaTickets import  ConexionTablaTickets
 from pydantic import BaseModel
 from models.ticketsModel import Ticket
 from datetime import datetime
